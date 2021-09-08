f0a63a0feef8bb550765043dd24f5a8ea19d6f7f
vivi zeng <vivizengjunyi@gmail.com>
2021-09-08 08:58:59 -0700

    update Sep 8th

--- src/components/singleMovie.js
+++ src/components/singleMovie.js
@@ -1,21 +1,26 @@
-
+import { AiFillStar } from 'react-icons/ai';
 
 const SingleMovie = ({ movieObj }) => {
     return (
         <div>
             <div className='single-movie'>
-                <div className='single-movie-backdrop' style={{backgroundImage: movieObj.backdrop_path && `url(https://image.tmdb.org/t/p/original/${movieObj.backdrop_path})`}}></div>
+                <div className='single-movie-backdrop' style={{ backgroundImage: movieObj.backdrop_path && `url(https://image.tmdb.org/t/p/original/${movieObj.backdrop_path})` }}></div>
                 <div className='single-movie-content'>
                     <div className='single-movie-poster'>
-                        {movieObj.poster_path == null ? 
-                        <img src="/" alt="no poster" />:
-                        <img src={`https://image.tmdb.org/t/p/w500/${movieObj.poster_path}`} alt={movieObj.title} />
-                    }
+                        {movieObj.poster_path == null ?
+                            <img src="/" alt="no poster" /> :
+                            <img src={`https://image.tmdb.org/t/p/w500/${movieObj.poster_path}`} alt={movieObj.title} />
+                        }
                     </div>
+                   
                     <div className='single-movie-info'>
-                        <h2>{movieObj.title}</h2>
+                        <p className='title'>{movieObj.title}</p>
+                        <div className='line'></div>
                         <div className="flexRow">
-                            <p>{movieObj.vote_average}</p>
+                            <div className='stick'>
+                                <AiFillStar className='start' />
+                                <p>{movieObj.vote_average}</p>
+                            </div>
                             <p>{movieObj.genre_ids}</p>
                             <p>{movieObj.release_date}</p>
                         </div>
