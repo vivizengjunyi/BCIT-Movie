ad733249efc78f5af24a9f77132b8cebf29f04c7
vivi zeng <vivizengjunyi@gmail.com>
2021-09-19 12:56:05 -0700

    update Sep 19th

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -3,7 +3,8 @@ const PageAbout = () => {
         <div className='page-about'>
             <div className='about-content'>
                 <div className='about-gomovie-name'>About GoMovie</div>
-                <p>GoMovie is a community showing movie database. Every piece of data has been added by our amazing community dating back to 2008. GoMovie's strong international focus and breadth of data is largely unmatched and something we're incredibly proud of. Put simply, we live and breathe community and that's precisely what makes us different.</p>
+                <p>GoMovie is a community showing movie database. Hundreds and thousands of movies information can be found here. You can search for any movies you are interested in and you can favorite them as well. In addition, you can choose any categories of movies to explore like popular, top-rated, now playing and upcoming. All the movie data are up-to-date.</p>
+                <p>GoMovie is a React JS project for educational purposes and is created by Vivi Zeng. </p>
             </div>
 
 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -12,7 +12,7 @@ function AppRouter() {
     <Router>
       <div className="wrapper">
         <Header />
-        <main>
+        <main className="main">
           <Switch>
             <Route path="/" exact><PageHome /></Route>
             <Route path="/index.html" exact><PageHome /></Route>
